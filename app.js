b253cceb0dd41898632cb7b2c40f80a48b40169c
ksee1230 <ksee1230@gmail.com>
2022-09-26 11:40:54 +0900

    container 명으로 접근 가능하도록 수정
    확인만 한 거라서 기능 구현 정확하게 다시 해야 함

--- app.js
+++ app.js
@@ -1,30 +1,33 @@
 const express = require('express');
+const es6Renderer = require('express-es6-template-engine');
 const app = express();
 const http = require('http');
 const path = require('path');
 const axios = require('axios');
+const ejs = require('ejs');
+const bodyParser = require('body-parser');
 const dest = 'http://rolling-server:8080';
 
-app.use(express.static('public'));
-
-app.get('/allnotes', (req, res) => {
-    console.log(dest);
-    axios.get(dest)
-    .then((sres) =>{
-        console.log("node: ", sres);
-        res.send(sres);
-    });
-});
-
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + "/public/index.html");
-})
+app.set('view engine', 'ejs');
+app.set('views', 'public');
+const router = express.Router()
+app.use(router)
 
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.sendFile(__dirname + "/public/index.html");
 })
 
+app.use(express.static('public'));
+router.use(bodyParser.urlencoded({ extended: false }))
+
+router.get("/", (req, res) => {
+    axios.get(dest)
+        .then(response => {
+            console.log(response)
+            res.render("index", { data: response.data })
+        })
+})
 
 server.listen(13000);
 
